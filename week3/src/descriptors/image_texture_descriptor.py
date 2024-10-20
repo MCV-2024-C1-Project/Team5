51464c1ab69201e6de7914320fd0c143e4079ac3
arnaubarrera <arnau6baroy@gmail.com>
2024-10-20 12:17:38 +0200

    WIP compute_lbp for block image

--- week3/src/descriptors/image_texture_descriptor.py
+++ week3/src/descriptors/image_texture_descriptor.py
@@ -2,31 +2,64 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from scipy.fftpack import dct
+from skimage.feature import local_binary_pattern
 
 from src.consts import ColorSpace
 from src.descriptors.base import Descriptor
 from typing import List, Callable, Optional
 
 from src.utils import zigzag
-from week2.src.metrics import DistanceType
-
+from src.metrics import DistanceType
 
 class ImageTextureDescriptor(Descriptor):
     def __init__(
             self,
             image,
             colorspace: ColorSpace = ColorSpace.RGB,
+            rows: int = 4,
+            columns: int = 4,
             texture_method: str = 'dct'
         ):
         super().__init__(image, colorspace)
         self.channels = image.shape[2]
-        self.values = self.compute_texture_descriptor(texture_method)
+        self.rows = rows
+        self.columns = columns
+        self.blocks = self.divide_image_into_blocks(rows, columns)
+        self.values = self.compute_lbp_blocks(radius=1)
 
     def compute_texture_descriptor(self, method=str):
         if method=='lbp':
             return self.compute_lbp()
         elif method=='dct':
             return self.compute_dct()
+        
+
+    def compute_lbp_blocks(self, radius):
+        """
+        Computes the Local Binary Pattern (LBP) image for each channel of an RGB image using a vectorized approach.
+
+        Parameters:
+            image (numpy.ndarray): Input RGB image.
+            radius (int): How far are the neighbours computed with the LBP
+
+        Returns:
+            lbp_image (numpy.ndarray): The resulting LBP image for the RGB channels.
+        """
+        blocks = self.blocks
+
+        n_points = 8 * radius
+
+        # Initialize the LBP image for each channel
+        lbp_blocks = np.zeros_like(blocks, dtype=np.uint8)
+
+        # Process each channel independently
+        num_channels = self.channels
+        for channel in range(num_channels):  # Loop over the three color channels
+            for block in blocks[:,:,channel]:
+                lbp_blocks = local_binary_pattern(block, radius, n_points)
+
+        return lbp_blocks
+    
 
     def compute_lbp(self):
         """
@@ -109,6 +142,67 @@ class ImageTextureDescriptor(Descriptor):
             descriptors.append(zigzag(dct_channel))
 
         return descriptors
+    
+
+    @staticmethod
+    def _divide_image_into_blocks(rows: int, columns: int, image):
+        """
+        Divides the image into a grid of blocks based on the number of rows and columns.
+
+        Parameters:
+            rows (int): The number of rows to divide the image into.
+            columns (int): The number of columns to divide the image into.
+
+        Returns:
+            blocks (list of lists): A 2D list (matrix) where each element is a block (sub-image)
+            of the original image.
+        """
+        # Get image dimensions
+        height, width = image.shape[:2]
+        # Compute size of each block
+        block_height = height // rows
+        block_width = width // columns
+
+        # Init a 2D list (matrix) to store the image blocks
+        blocks = [[None for _ in range(columns)] for _ in range(rows)]
+
+        # Loop thorugh each block position
+        for i in range(rows):
+            for j in range(columns):
+
+                image_block = image[(i*block_height):(i*block_height) + block_height,
+                                        (j*block_width):(j*block_width) + block_width]
+
+                blocks[i][j] = image_block
+
+        return blocks
+
+
+    def divide_image_into_blocks(self, rows: int, columns: int):
+        """
+        Divides the image into a grid of blocks based on the number of rows and columns.
+
+        Parameters:
+            rows (int): The number of rows to divide the image into.
+            columns (int): The number of columns to divide the image into.
+
+        Returns:
+            blocks (list of lists): A 2D list (matrix) where each element is a block (sub-image)
+            of the original image.
+        """
+        image_blocks = self._divide_image_into_blocks(rows, columns, self.image)
+        return image_blocks
+
+    def plot_image_blocks(self):
+        fig, axes = plt.subplots(self.rows, self.columns, figsize=(self.rows-1, self.columns-1))
+
+        for i in range(self.rows):
+            for j in range(self.columns):
+                axes[i, j].imshow(self.blocks[i][j])
+                axes[i, j].axis('off')
+
+        plt.tight_layout()
+        plt.show()
 
     
     def _compute_similarity_or_distance(self, descriptor2: 'ImageTextureDescriptor', func: Callable):
